77941c642660f0f221b3d38affa5ebfedc6f4719
mcc <costanza+git@gmail.com>
2021-06-02 11:12:58 -0700

    add voter routes

--- routes/apiRoutes/candidateRoutes.js
+++ routes/apiRoutes/candidateRoutes.js
@@ -43,11 +43,13 @@ router.get('/candidate/:id', (req, res) => {
 
 // create a candidate
 router.post('/candidate', ({ body }, res) => {
+  // data validation
   const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
   if (errors) {
     res.status(400).json({ error: errors });
     return;
   }
+
   const sql = `INSERT INTO candidates (first_name, last_name, industry_connected) VALUES (?, ?, ?)`;
   const params = [body.first_name, body.last_name, body.industry_connected];
 
@@ -95,8 +97,7 @@ router.put('/candidate/:id', (req, res) => {
 // delete a candidate from the list
 router.delete('/candidate/:id', (req, res) => {
   const sql = `DELETE FROM candidates WHERE id = ?`;
-  const params = [req.params.id];
-  db.query(sql, params, (err, result) => {
+  db.query(sql, req.params.id, (err, result) => {
     if (err) {
       res.statusMessage(400).json({ error: res.message });
     } else if (!result.affectedRows) {
